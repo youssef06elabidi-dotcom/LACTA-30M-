46d008932a6136500b8b738effc1350922ccd904
youssef06elabidi-dotcom <youssef06elabidi@gmail.com>
2026-08-03 16:58:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,19 @@
-const CACHE_NAME = "lacta-jibal-v1";
+const CACHE_NAME = "lacta-jibal-v3";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./images/m2_m10.jpg",
+  "./images/m6_m7.jpg",
+  "./images/v122.jpg",
+  "./images/v159.jpg",
+  "./images/v131_new.jpg",
+  "./images/v143.jpg",
+  "./images/v117.jpg",
+  "./images/ft41.jpg",
+  "./images/v193a.jpg"
 ];
 
 self.addEventListener("install", (event) => {
